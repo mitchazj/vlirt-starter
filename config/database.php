e4fafd2b75cc369b81349ce333703bc62c8e9b1c
Mitchell Johnson <mitchellazj@gmail.com>
2021-09-21 02:52:38 +0000

    hmmm

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'planetscale'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
